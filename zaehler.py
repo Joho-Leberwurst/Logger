ab8ab1cab5fe1f3ceb41b0055aa3ea6954ea56f9
Joho-Leberwurst <amatrix@posteo.de>
2015-12-20 19:51:39 +0100

    Update zaehler.py
    Add bouncetime = 100

--- zaehler.py
+++ zaehler.py
@@ -6,25 +6,19 @@ import os
 #set GPI Mode
 GPIO.setmode(GPIO.BCM)
 
-phaseOne     = 13
+phaseOne     = 4
 uuid = "1d083960-3e9e-11e5-ab65-c39324b86969"
+
+GPIO.setup(phaseOne, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  
 
 def inter( ):
     try:  
-        GPIO.wait_for_edge(phaseOne, GPIO.RISING)  
+        GPIO.wait_for_edge(phaseOne, GPIO.RISING, bouncetime = 100)  
         print "Signal"
-        os.system("wget -O /dev/null http://localhost/middleware.php/data/" + uuid + ".json?operation=add&value=1 --nv &") 
+        os.system("wget -O /dev/null http://localhost/middleware.php/data/" + uuid + ".json?operation=add&value=1") 
         inter( )
     except KeyboardInterrupt:  
         GPIO.cleanup( )  
 
-
-# set pin to pull_up_down
-GPIO.setup(phaseOne, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  
-try:  
-    GPIO.wait_for_edge(phaseOne, GPIO.RISING)  
-    os.system("wget -O /dev/null http://localhost/middleware.php/data/" + uuid + ".json?operation=add&value=1 --nv &") 
-    inter( )
-except KeyboardInterrupt:  
-    GPIO.cleanup( )  
+inter( )
     
